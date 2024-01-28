a6e3c8afd11b915cfa602c1287bab9013e1e6007
Girish <girish.170902@gmail.com>
2024-01-28 14:44:45 +0530

    Configuration Properties

---
+++ src/main/java/com/springboot/controller/ConfigurationPropertyController.java
@@ -0,0 +1,27 @@
+package com.springboot.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.springboot.property.Property;
+
+@RestController
+@RequestMapping("/confProperty")
+public class ConfigurationPropertyController {
+	
+	@Autowired
+	private Property property;
+	
+	@GetMapping
+	public List<String> fetchConfigurationProperties() {
+		List<String> propertyValues = new ArrayList<>();
+		propertyValues.add(String.valueOf(property.getNoOfStudents()));
+		propertyValues.add(property.getSchoolName());
+		return propertyValues;
+	}
+}
